7a9cffdb47b8c2d5ecb6389c259cd554f276121c
bf0880-boop <bf0880@est.ort.edu.ar>
2026-05-07 10:17:56 -0300

    CajaMovement implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,11 +13,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow) ||Input.GetKeyDown(KeyCode.D))
+        if((Input.GetKeyDown(KeyCode.RightArrow) ||Input.GetKeyDown(KeyCode.D)) && transform.position.x < 1)
         {
             transform.Translate(2,0,0);
         } 
-        else if (Input.GetKeyDown(KeyCode.LeftArrow)||Input.GetKeyDown(KeyCode.A))
+        else if ((Input.GetKeyDown(KeyCode.LeftArrow)||Input.GetKeyDown(KeyCode.A)) && transform.position.x > -1)
         {
             transform.Translate(-2,0,0);
         }
