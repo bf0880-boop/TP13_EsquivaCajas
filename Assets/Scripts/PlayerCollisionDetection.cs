0c32c09a9840306e121d8ba82eeac83aba24a34d
bf0880-boop <bf0880@est.ort.edu.ar>
2026-05-07 10:33:22 -0300

    Collision realized

---
+++ Assets/Scripts/PlayerCollisionDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+         void OnCollisionEnter(Collision col){
+       if (col.gameObject.CompareTag("Caja"))
+        {
+            // Destruye el player (este objeto)
+            Destroy(gameObject);
+        }
+        }
+}
